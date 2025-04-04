6872db7fe95592ba39a3c54f28fe99ad3d2b4afd
kidow <dongwook.kim@teamtoodle.com>
2025-04-05 00:12:41 +0900

    chore: trim code

--- app/theme.tsx
+++ app/theme.tsx
@@ -4,27 +4,22 @@ import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
-export default function Theme() {
+export default function Theme(): React.ReactElement {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
 
-  // useEffect는 클라이언트 측에서만 실행
   useEffect(() => {
-    console.log(theme)
     setMounted(true)
   }, [])
 
-  if (!mounted) {
-    return null
-  }
-
+  if (!mounted) return <></>
   return (
     <button
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       className="rounded-md p-2 fixed top-2 right-2"
       aria-label="테마 변경"
     >
-      {theme === 'dark' ? <Sun size={20} /> : <Moon size={20} />}
+      {theme === 'dark' ? <Moon size={20} /> : <Sun size={20} />}
     </button>
   )
 }
